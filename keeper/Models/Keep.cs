97cbc20a153aeaa0a170e4d68b2700760828923f
Payton Boone <paytonboone30@gmail.com>
2023-03-31 12:58:19 -0600

    keep model and controller for posting is done

---
+++ keeper/Models/Keep.cs
@@ -0,0 +1,14 @@
+namespace keeper.Models
+{
+    public class Keep
+    {
+        public int Id { get; set; }
+        public string CreatorId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int Views { get; set; }
+        public Profile Creator { get; set; }
+        public int KeptId { get; set; }
+    }
+}
---
+++ keeper/Services/KeepsService.cs
@@ -0,0 +1,10 @@
+namespace keeper.Services
+{
+    public class KeepsService
+    {
+        internal Keep CreateKeep(Keep keepData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
